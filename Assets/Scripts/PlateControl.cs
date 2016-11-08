d5d3137881656ac0930d43e691145e772f808278
Yaroslav <yaroslavtereshchuk@gmail.com>
2016-11-08 19:15:58 +0200

    commit

---
+++ Assets/Scripts/PlateControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlateControl : MonoBehaviour {
+    Rigidbody2D rb2d;
+    public int speed = 100;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+        else if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Wall")
+        {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+    }
+}
---
+++ Assets/Scripts/SquareControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquareControl : MonoBehaviour {
+    Rigidbody2D rb2d;
+    public float speed = 500;
+    float beg_x_position = 0, beg_y_position = 0;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        beg_x_position = rb2d.position.x;
+        beg_y_position = rb2d.position.y;
+        rb2d.velocity = new Vector2(speed, speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "RightWall" || col.gameObject.name == "LeftWall")
+            rb2d.velocity = new Vector2(-rb2d.velocity.x, rb2d.velocity.y);
+        else if (col.gameObject.name == "UpWall" || col.gameObject.name == "Plate")
+            rb2d.velocity = new Vector2(rb2d.velocity.x, -rb2d.velocity.y);
+        else if (col.gameObject.tag == "Floor") Spavn();
+        else if (col.gameObject.tag == "Enemy") {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, -rb2d.velocity.y);
+            Destroy(col.gameObject); 
+        }
+    }
+
+    void Spavn()
+    {
+        rb2d.position = new Vector3(beg_x_position, beg_y_position, rb2d.transform.position.z);
+        rb2d.velocity = new Vector2(speed, speed);
+    }
+}
